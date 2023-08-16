da55e6504a8558a7859079ac17758c058fc06d0a
Martin Chocho <mchocho@technisys.com>
2023-08-16 16:22:24 -0300

    Static methods and inheritance in classes

--- Classes.js
+++ Classes.js
@@ -11,8 +11,29 @@ class SuperHero {
     powerUp() {
         this.strength += 5;
     }
+
+    //Métdos staticos, son funciones que no están atadas a los objetos creados por la clase, si no a la clase misma
+    //En este ejemplo todos los super heroes son buenos entonces toda la clase SuperHero retorna que son buenos.
+    static goodHero() {
+        return true;
+    }
 }
 
-let hero = new SuperHero("Thor", 10, 8, "Mjolnr", true);
+//Herencia hacia abajo, Human es hija de SuperHero
+class Human extends SuperHero {
+    constructor(healthPoints, ...superHero){
+        super(...superHero);
+        this.healthPoints = healthPoints;
+    }
+}
+
+const thor = ["Thor", 10, 8, "Mjolnr", true];
+const hero = new SuperHero(...thor);
 hero.powerUp();
 console.log(hero);
+
+const ironMan = [20,"Iron man", 5, 6, "The suit", false];
+const hero2 = new Human(...ironMan);
+console.log(hero2);
+
+console.log(SuperHero.goodHero());
